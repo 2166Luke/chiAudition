46af843017e6b1a74445129f08a1ecc0ea47b67d
2166Luke <99983789+2166Luke@users.noreply.github.com>
2024-06-18 02:19:34 +0000

    finished intializing flask app settings

--- app.py
+++ app.py
@@ -1,10 +1,28 @@
 import os
 
 from cs50 import SQL
-from flask import Flask, render_template, request
+from flask import Flask, render_template, redirect, session, request
+from flask_session import Session
+from werkzeug.security import generate_password_hash, check_password_hash
 
 # Configure application
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")
-app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
+# Configure CS50 Library to use SQLite database
+db = SQL("sqlite:///auditions.db")
+
+# Ensure responses aren't cached
+@app.after_request
+def after_request(response):
+    response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    response.headers["Expires"] = 0
+    response.headers["Pragma"] = "no-cache"
+    return response
+
+@app.route("/")
+def index():
+    return render_template("index.html")
