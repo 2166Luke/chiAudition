824641943d00f75bdbefc0ac747b74bb09ae1786
2166Luke <99983789+2166Luke@users.noreply.github.com>
2024-06-25 19:26:06 +0000

    -

--- scraper.py
+++ scraper.py
@@ -15,8 +15,10 @@ title = re.sub(" - Theatre In Chicago", "", title) # Remove " - Theatre in Chica
 def scrapeTIC():
     baseUrl = "https://www.theatreinchicago.com/auditions/"
     homeHtml = urlopen(baseUrl).read().decode('latin-1')
-    pattern = r'<div class="post-title">\s*<a href="industrydetail\.php\?AuditionID=(\d+)">\s*(.*?)</a>\s*</div>'
+
+    pattern = r'<div class="post-content">\s*<div class="post-title">\s*<a href="industrydetail\.php\?AuditionID=(\d+)">\s*(.*?)</a>\s*</div>\s*<div class="theatre-name">\s*(.*?)</div>\s*<div class="post-meta">\s*(.*?)</div>\s*</div>'
     match_results = re.findall(pattern, homeHtml, re.IGNORECASE)
+    print(match_results)
     for audition in match_results:
         # Extract audition ID
         auditionId = audition[0]
@@ -28,6 +30,7 @@ def scrapeTIC():
         match_results = re.search(pattern, auditionHtml, re.IGNORECASE)
         if match_results:
             auditionTitle = match_results.group(1)
+            print(f"Audition Title: {auditionTitle}")
 
         # Extract the company
         pattern = r'<div class=\"post-title\">\s*(.*?)\s*</div>'
@@ -36,7 +39,12 @@ def scrapeTIC():
             company = match_results.group(1)
             company = re.sub("<.*?>", "", company) # Remove HTML tags
         
-        
+        # Extract the date and time
+        pattern = r'<div class'
+
+def pushAudition(title, company, date, time, otherDates, location, description, pay, eqStatus, personnel, source):
+    
+    print(f"Title: {title}")
 
 
 scrapeTIC()
