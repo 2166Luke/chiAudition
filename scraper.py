dcd08ccbd94e6f3762e26dd3d8f38c55437a0533
2166Luke <99983789+2166Luke@users.noreply.github.com>
2024-07-03 19:13:55 +0000

    finished the regex expression for scraping data off the TIC auditions landing page

--- scraper.py
+++ scraper.py
@@ -1,45 +1,29 @@
 from urllib.request import urlopen
 import re
 
-url = "https://www.theatreinchicago.com/auditions/"
-
-html = urlopen(url).read().decode('latin-1')
-
-pattern = "<title.*?>.*?</title.*?>"
-match_results = re.search(pattern, html, re.IGNORECASE)
-title = match_results.group()
-title = re.sub("<.*?>", "", title) # Remove HTML tags
-title = re.sub(" - Theatre In Chicago", "", title) # Remove " - Theatre in Chicago"
-
-
 def scrapeTIC():
     baseUrl = "https://www.theatreinchicago.com/auditions/"
     homeHtml = urlopen(baseUrl).read().decode('latin-1')
 
-    pattern = pattern = r'<div class="post-content">\s*<div>(.*?)</div>\s*<div class="post-title">\s*<a href="industrydetail\.php\?AuditionID=(\d+)">\s*<strong>(.*?)</strong>\s*</a>\s*</div>\s*<div class="theatre-name">\s*(.*?)\s*</div>\s*<div class="post-meta">\s*<b>(.*?)</b>\s*(.*?)</div>'
+    pattern = r'<div class=\"post-content\">\s*<div>(.*?)</div>\s*<div class=\"post-title\">\s*<a href=\"industrydetail.php\?AuditionID=(\d+)\">\s*<strong>(.*?)</strong>\s*</a>\s*</div>\s*<div class=\"theatre-name\">\s*(.*?)\s*</div>\s*<div class=\"post-meta\">\s*<b>.*</b>(?:\s*(.*?)\s*<br><br>)?\s*</div>'
     match_results = re.findall(pattern, homeHtml, re.IGNORECASE)
-    print(match_results)
     for audition in match_results:
-        # Extract audition ID
+        # Create variables from data on the auditions landing page
+        company = audition[0]
         auditionId = audition[1]
+        title = audition[2]
+        status = audition[3]
+        if audition[4] != '':
+            dates = audition[4]
+        else:
+            dates = "N/A"
+        print(company, "    ", title, "    ", status, "    ", dates)
 
         # Route to the individual audition page and extract the title
         auditionUrl = baseUrl + "industrydetail.php?AuditionID=" + auditionId 
         auditionHtml = urlopen(auditionUrl).read().decode('latin-1')
-        pattern = r'<div class="subtitle">\s*(.*?)\s*</div>'
-        match_results = re.search(pattern, auditionHtml, re.IGNORECASE)
-        if match_results:
-            auditionTitle = match_results.group(1)
-
-        # Extract the company
-        pattern = r'<div class=\"post-title\">\s*(.*?)\s*</div>'
-        match_results = re.search(pattern, auditionHtml, re.IGNORECASE)
-        if match_results:
-            company = match_results.group(1)
-            company = re.sub("<.*?>", "", company) # Remove HTML tags
         
-        # Extract the date and time
-        pattern = r'<div class='
+        
 
 def pushAudition(title, company, date, time, otherDates, location, description, pay, eqStatus, personnel, source):
     
